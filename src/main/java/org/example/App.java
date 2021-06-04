e9e484a68550aef20a96a620db4d6fb8050cd20a
Anisha Desktop <anisharanjan55@gmail.com>
2021-06-04 12:23:15 -0400

    Solution w/ comment

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,9 +8,21 @@ public class App
 {
     public static void main( String[] args ) {
 
-        
+        Scanner in = new Scanner(System.in);
 
+        System.out.print("What is the principal amount? ");
+        int P = in.nextInt();
+        System.out.print("What is the rate? ");
+        float r = in.nextFloat();
+        double rate = r / 100.0;
+        System.out.print("What is the number of years? ");
+        int t = in.nextInt();
+        System.out.print("What is the number of times the interest is compounded per year? ");
+        int n = in.nextInt();
 
+        double A = P * Math.pow(1.0 + rate / n, n * t);
+
+        System.out.printf("$%d invested at %.1f%% for %d years compounded %d times per year is $%.2f.", P, r, t, n, A);
 
     }
 }
